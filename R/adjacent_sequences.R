17e3ce3671855580a523fb47132f18a44244577e
Tomrrr1 <76951517+Tomrrr1@users.noreply.github.com>
2024-03-08 08:38:43 +0000

    Edit adjacent_sequences to consider the start and end points of each chromosome. Other minor changes. [skip ci]

--- R/adjacent_sequences.R
+++ R/adjacent_sequences.R
@@ -1,29 +1,39 @@
 #' Generate sequences immediately adjacent to peaks
 #'
 #' \code{adjacent_sequences()} generates sequences adjacent to each peak in
-#' "peaks". These sequences will be used as a reference when calculating the
-#' proportion of peaks with a motif.
+#' "peaks". These adjacent sequences will have the same width as the peaks.
+#' These are used as a reference when calculating the proportion of peaks with
+#' a motif. Where a peak is at the boundaries of a chromosome the adjacent
+#' sequence width will not go beyond the boundaries of the chromosome.
 #'
 #' @param peaks A GRanges peak object
 #'
+#' @import GenomicRanges
+#' @importFrom GenomeInfoDb seqlengths
+#'
 #' @keywords internal
-adjacent_sequences <- function(peaks){
-  # Calculate the width of each peak
+adjacent_sequences <- function(peaks) {
+  # Retrieve the width of each peak
   peak_widths <- GenomicRanges::width(peaks)
 
-  # Randomly choose direction for each peak: TRUE for upstream, FALSE for downstream
+  # Retrieve chromosome lengths for boundary checks
+  chrom_sizes <- GenomeInfoDb::seqlengths(BSgenome.Hsapiens.UCSC.hg38)
+
+  # Randomly choose the side that each adjacent sequence will be placed
   is_upstream <- sample(c(TRUE, FALSE), size = length(peaks), replace = TRUE)
 
-  # Update starts and ends based on direction
   for (i in seq_along(peaks)) {
+    chrom <- as.character(seqnames(peaks)[i])
+    chrom_length <- chrom_sizes[chrom]
+
     if (is_upstream[i]) {
-      # control sequence will be upstream
+      # Control sequence will be upstream
       new_end <- start(peaks)[i] - 1
-      new_start <- new_end - peak_widths[i] + 1
+      new_start <- max(1, new_end - peak_widths[i] + 1)
     } else {
-      # controls sequence will be downstream
+      # Control sequence will be downstream
       new_start <- end(peaks)[i] + 1
-      new_end <- new_start + peak_widths[i] - 1
+      new_end <- min(chrom_length, new_start + peak_widths[i] - 1)
     }
 
     # Update the peak coordinates
--- tests/testthat/test-peak_proportion.R
+++ tests/testthat/test-peak_proportion.R
@@ -1,3 +1,26 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("list outputted by peak_proportion()", {
+  peak_file <- system.file("extdata",
+                           "rep1_peaks.narrowPeak",
+                           package = "MotifStats")
+  data("creb_motif", package = "MotifStats")
+
+  res <- peak_proportion(
+    peak_file = peak_file,
+    pwm = creb_motif,
+    plot = FALSE,
+    min_score = 0.8
+  )
+
+  expect_true(is.list(res))
 })
+
+ctcf_peaks <- "read1_no_ctrl_01_peaks.narrowPeak"
+motif_pwm <- read_motif_file(motif_file = "MA1930.2.jaspar",
+                             file_format = "jaspar")
+
+peak_proportion(peak_file = ctcf_peaks,
+                pwm = motif_pwm,
+                min_score = 0.6)
+
+
+
